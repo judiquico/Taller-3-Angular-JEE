603c8079ddc10adc369420b63b6e108afb5a22d8
Juan Diego <Juan Diego@JuanDiegoPc>
2018-03-13 09:00:20 -0500

    Se añada el repositorio el projecto usando Angular

---
+++ web/js/Modelo.js
@@ -0,0 +1,55 @@
+
+
+var idFacultad=3;
+var facultades=[
+    {"id":1, "nombre":"Ingenieria"},
+    {"id":2, "nombre":"Licenciatura"}
+   
+
+];
+var idCarrera=1;
+var carreras=[
+    
+];
+
+var idMunicipio=4;
+var municipios=[
+    {"id":1, "nombre":"Tunja"},
+    {"id":2, "nombre":"Duitama"},
+    {"id":3, "nombre":"Sogamoso"},
+];
+
+var idProfesor=12;
+var profesor=[
+    {"id":1,"nombre":"Helver","apellido":"Valero","documento":"654554"},
+    {"id":2,"nombre":"Otalora","apellido":"pedro","documento":"4053289"},
+    {"id":3,"nombre":"Hernan","apellido":"Felipe","documento":"234354"},
+    {"id":4,"nombre":"Sebastian","apellido":"Rojas","documento":"343565"},  
+];
+
+
+var idEstudiante=13;
+var estudiante=[
+    {"id":1,"nombre":"oswaldo","apellido":"Boada","codigo":"654554","documento":"343565","fecha":d=new Date("October 13, 2018 11:13:00")},
+    {"id":2,"nombre":"lola","apellido":"Perez","codigo":"11111","documento":"4546","fecha":d=new Date("November 13, 2020 11:13:00")},
+    {"id":3,"nombre":"facundo","apellido":"Garces","codigo":"4464","documento":"3454","fecha":d=new Date("December 13, 2019 11:13:00")},
+    {"id":4,"nombre":"Sor","apellido":"Rita","codigo":"6565","documento":"234","fecha":d},
+];
+
+var d = new Date("October 13, 2018 11:13:00");
+
+
+var idMateria=13;
+var materia=[
+    {"id":1,"nombre":"Diseño Web","creditos":3},
+    {"id":2,"nombre":"Algorutmos y Programacion","creditos":4},
+    {"id":3,"nombre":"Programacion I","creditos":4},
+    {"id":4,"nombre":"Programacion II","creditos":3},
+    {"id":5,"nombre":"Programacion III","creditos":4},
+    {"id":6,"nombre":"Bases I","creditos":5},
+    
+];
+
+
+
+
